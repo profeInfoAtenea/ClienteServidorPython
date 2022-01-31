c9989d419b1f51efc8a14ad79fc0b3db25aa8653
profeInfoAtenea <90715116+profeInfoAtenea@users.noreply.github.com>
2022-01-31 14:17:18 +0100

    Update tcpserver.py

--- tcpserver.py
+++ tcpserver.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
 #tcpserver.py
 import socket
-host = "localhost"
+host = "localhost" #cambiar por la IP
 port = 1339
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET -> IPV4 , SOCK_STREAM -> TCP
 sock.bind((host, port))
 sock.listen(10)
 print("Servidor escuchando peticiones en el puerto " + str(port))
 while(1):
     cli, add = sock.accept()
     buf = cli.recv(1024)
-    print("Recibido: ", buf)
-    if buf=="Hello":
-        cli.send("Message from the server \n")
+    texto_recibido = ""
+    try:
+        texto = buf.decode('utf-8')
+        print("Recibido: ", texto)
+    except:
+        print("error")
+   
+
+    if buf=="Hola":
+        texto_enviar = "¡Hola!, ¿Qué tal estás?"
+        cli.send(texto_enviar.encode('utf-8')
     cli.close()
